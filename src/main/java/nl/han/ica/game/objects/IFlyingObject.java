29344601289ecb3e8a4bd2b0a65268f9b0164565
Stefan Teunissen <stefanteunissen1@gmail.com>
2020-10-19 14:16:48 +0200

    Level progess onthouden, en comments

--- src/main/java/nl/han/ica/game/objects/IFlyingObject.java
+++ src/main/java/nl/han/ica/game/objects/IFlyingObject.java
@@ -1,6 +1,13 @@
 package nl.han.ica.game.objects;
 
 public interface IFlyingObject  {
+    /**
+     * Wat er moet gebeuren als het object aangeraakt wordt door de player
+     */
     void hit();
+
+    /**
+     * De condities voor wanneer het obejct verwijderdt moet worden
+     */
     void checkIfOffScreen();
 }
